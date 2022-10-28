2f208c17c0e3825de824a187182b9c0d7d709cf4
waiho wong <x00185050@mytudublin.ie>
2022-10-28 11:52:38 +0100

    lab 9 Part 1 commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
+from .models import Profile
 
 
 # Register your models here.
@@ -12,3 +13,4 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['email', 'username', 'age', 'is_staff',]
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Profile)
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DetailView
 from .forms import CustomUserCreationForm
+from .models import Profile
 
 # Create your views here.
 
@@ -8,3 +9,13 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class ProfileEditView(UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile.html'
+    fields = ['fav_author']
+    success_url = reverse_lazy('home')
+
+class ProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
