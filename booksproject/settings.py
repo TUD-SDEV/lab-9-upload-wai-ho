30d9278c4695735fecf7f55e3da0c476c7954d87
waiho wong <x00185050@mytudublin.ie>
2022-11-10 11:43:11 +0000

    lab 9 part 4 commit

--- booksproject/settings.py
+++ booksproject/settings.py
@@ -144,3 +144,6 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
