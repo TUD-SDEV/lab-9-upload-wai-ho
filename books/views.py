07ac1524f15385544c35456bb40b8fcc0c62fc5a
waiho wong <x00185050@mytudublin.ie>
2022-11-10 12:00:53 +0000

    lab 9 part 5 commit

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Book
+from django.db.models import Q
 # Create your views here.
 
 class BookListView(ListView):
@@ -10,3 +11,14 @@ class BookListView(ListView):
 class BookDetailView(DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
